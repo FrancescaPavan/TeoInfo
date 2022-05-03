8c9527eacd9a4457eb13725b7229c66d8c5e95d5
Francesca Pavan <francescapavan7620@gmail.com>
2022-05-03 15:45:03 -0300

    ejercicio 4c

---
+++ 4c.py
@@ -0,0 +1,61 @@
+import numpy as np
+import random
+
+from sqlalchemy import false, true
+
+def calc_sig_valor(vAnt):
+    m = np.array([[0.5, 0.33, 0],[1,0.66,1],[1,1,1]])
+    vAleatorio = random.random()
+    for i in range(3):
+        if vAleatorio <= m[i,vAnt]:
+            return i
+
+def primer_valor():
+    vEst = np.array([0.33,0.83,1])
+    vAleatorio = random.random()
+    for i in range(3):
+        if vAleatorio <= vEst[i]:
+            return i
+
+def calcular_probabilidades(n,exitos):
+    # print('en calcular')
+    # print(exitos)
+    # print(n)
+    prob=np.array([0.0,0.0,0.0])
+    for i in range(3):
+        prob[i] = exitos[i]/float(n)
+        # print(prob[i])
+    return prob
+
+def converge(exitos,ant):
+    conv = (np.subtract(ant,exitos) < 0.0000001)
+    #print(np.subtract(ant,exitos) < 0.00001)
+    return(np.all(conv))
+    
+def dos_consecutivos():
+    n=0
+    exitos=np.array([0,0,0])
+    probActual=np.array([0,0,-1])
+    consecutivos=0
+    probAnterior=np.array([0,0,0])
+    vAnterior=primer_valor()
+    while not(converge(probActual, probAnterior)) or n < 10000:
+        print(n)
+        print(converge(probActual, probAnterior))
+        vActual=calc_sig_valor(vAnterior)
+        print("Ant: "+str(vAnterior)+"   Actual: "+str(vActual))
+        probAnterior = probActual
+        n+=1
+        if (vAnterior==vActual):
+            exitos[vActual] += 1
+            consecutivos += 1
+            probActual = calcular_probabilidades(n,exitos)
+            print(probActual)
+        
+        vAnterior=vActual
+    return probActual
+
+print(dos_consecutivos())
+
+
+        
